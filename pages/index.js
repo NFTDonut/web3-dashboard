aac9b6b4e5661f65b03c61eab5f3ad4b5fb37865
Caleb Rider <donutclubnft@gmail.com>
2022-07-06 20:54:40 -0400

    added vercel env variables and deployed

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { ethers, utils } from 'ethers'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Navbar from '../components/Navbar'
@@ -6,7 +7,25 @@ import GasCard from '../components/GasCard'
 import PriceCard from '../components/PriceCard'
 import BalanceCard from '../components/BalanceCard'
 
+
 export default function Home() {
+
+  const ALCHEMY_API = process.env.ALCHEMY_API;
+  const ETHERSCAN_API = process.env.ETHERSCAN_API;
+
+  
+
+  provider = new ethers.providers.JsonRpcProvider("homestead", process.env.ALCHEMY_API);
+  etherscanProvider = new ethers.providers.EtherscanProvider("homestead", process.env.ETHERSCAN_API);
+
+  const getValues = async () => {
+    gasPrice = await provider.getGasPrice();
+    gasPriceFormatted = utils.formatUnits(gasPrice, "gwei");
+    console.log(gasPriceFormatted);
+  }
+
+  getValues();
+
   return (
     <div>
       <Head>
