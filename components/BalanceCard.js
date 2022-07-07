3c49628eefe6d7f8dabc5aefbff63976fbdf6d95
Caleb Rider <donutclubnft@gmail.com>
2022-07-07 18:32:41 -0400

    styled cards

--- components/BalanceCard.js
+++ components/BalanceCard.js
@@ -4,12 +4,22 @@ export default function BalanceCard() {
 
     return(
         <div className={styles.card}>
-            <div className={styles.cardTitle}>
+            <div className={styles.walletCardTitle}>
                 <h1>Wallet Data:</h1>
             </div>
-            <div className={styles.cardContent}>
-                <input placeholder='Wallet Address'></input>
-                <h2>Hi</h2>
+            <div className={styles.walletCardContent}>
+                <div className={styles.interaction}>
+                    <input placeholder='Enter a Wallet Address'></input>
+                    <button className={styles.submitButton}>Submit</button>
+                </div>
+                <div className={styles.results}>
+                    <h3>Balance (ETH):</h3>
+                    <h2>fs</h2>
+                </div>
+                <div className={styles.results}>
+                    <h3>Latest Transaction:</h3>
+                    <h2>gs</h2>
+                </div>
             </div>
         </div>
     );
